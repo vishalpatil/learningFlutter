f43e3fd3b18bea73dafb7ab0be5c42c4bf1f46fc
Vishal Patil <vishal@deedeelabs.org>
2020-09-17 00:12:54 +0530

    hello world with styling and font addition

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,37 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold( )
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('my first app'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Text(
+          'hello ninjas!',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+            fontFamily: 'IndieFlower',
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Text('click'),
+        backgroundColor: Colors.red[600],
+    ),
+    ),
   ));
+}
+
+
+class test extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
